790c5c1a768822c41931a081a6e2773de45de58f
Gonzalo Acosta <gonzalo.acosta@semperti.com>
2021-03-22 14:45:23 -0300

    change mkdir.sh

--- scripts/mkdir.sh
+++ scripts/mkdir.sh
@@ -2,5 +2,5 @@ mkdir -p /web
 curl -O https://bootstrap.pypa.io/get-pip.py
 yum -y install python3 python3-pip
 python3 get-pip.py --user
-python3 -m pip install Flask
+python3 -m pip install Flask requests
 
