bdd4ae5ad193e9ba20cb11ba9e8535f15b795998
Gonzalo Acosta <gonzalo.acosta@semperti.com>
2021-03-22 15:09:18 -0300

    change mkdir.sh

--- scripts/stop_flask.py
+++ scripts/stop_flask.py
@@ -1,3 +1,5 @@
-import requests
+#!/usr/bin/python3
+from botocore.vendored import requests
+#import requests
 
 requests.post("http://127.0.0.1/shutdown")
--- scripts/stop_flask1.sh
+++ scripts/stop_flask1.sh
@@ -1,3 +1,1 @@
-pip3 install requests
-pip3 install requests --user
 python3 /web/scripts/stop_flask.py
